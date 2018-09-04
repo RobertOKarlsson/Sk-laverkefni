6125172c1cc9c7fe4bca5b2c3ea255e624e0a530
robbi <robbi.orn.karlsson@gmail.com>
2018-09-04 12:14:43 +0000

    varibles added for number sequence and code finished

--- sequence.py
+++ sequence.py
@@ -1,6 +1,13 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
-counter = 0
-for n in range(n):
-    counter + 1
 
-    print(range(15))
+num1 = 0
+num2 = 1
+num3 = 0
+summa = num1 + num2 + num3
+for n in range(0,n):
+    summa = num1 + num2 + num3
+    num1 = num2
+    num2 = num3
+    num3 = summa
+    print(summa)
+    
